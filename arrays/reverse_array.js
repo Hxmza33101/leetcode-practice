bac8ca353fa27580adfe2957ff6e5f279654c2fa
Hxmza33101 <hkmughal2007@gmail.com>
2026-02-16 12:55:19 +1100

    Added meaningful comments to reverse_array.js. A useful and quicker in built method of js that reverses arrays.

--- arrays/reverse_array.js
+++ arrays/reverse_array.js
@@ -9,5 +9,7 @@ var reverse = function(s) {
     }
 }
 
+//Another way to do it is to use the built in .reverse() method for javascript. It is still O(n) time complexity, it is just quicker.
+
 /*This is an in place reverse array function. It reverses the array by starting at both ends and simultaneously switches
  the position of both elements. This effectively reverses the array as once i is greater than l, the array is rotated */
