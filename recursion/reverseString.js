17c7854229746f005a119d0b802b03b91991fdc9
Hxmza33101 <hkmughal2007@gmail.com>
2026-03-09 20:49:58 +1100

    Added comments to a few files in the repo

--- recursion/reverseString.js
+++ recursion/reverseString.js
@@ -1,3 +1,5 @@
+// This function works by recursively checking if the string is empty, and if it isnt, returning a recursion call of the function and concattenating the rest of the string
+
 function reverse(str) {
   if (str === "") return "";
   return reverse(str.slice(1)) + str[0];
