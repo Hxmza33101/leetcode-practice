1b73fcd072356d07d50de48d24860c194038509a
Hxmza33101 <hkmughal2007@gmail.com>
2026-03-23 21:10:21 +1100

    completed a problem provided by chatgpt in which 2 sorted arrays are merged in ascending order

---
+++ arrays/merge_sorted_arrays.js
@@ -0,0 +1,28 @@
+function merge(arr, arr2) {
+    let result = [];
+    
+    let i = 0;
+    let j = 0;
+
+    while (i < arr.length && j < arr2.length) {
+        if (arr[i] < arr2[j]) {
+            result.push(arr[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr.length) {
+        result.push(arr[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
