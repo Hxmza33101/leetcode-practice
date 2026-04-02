fd93f23b40f1161e281c35e8959a639309455ad7
Hxmza33101 <hkmughal2007@gmail.com>
2026-04-02 19:27:10 +1100

    Am now going to learn python alongside js for internships. Split the repo into 2 folders by language

---
+++ python/basic_syntax_practice.py
@@ -0,0 +1,50 @@
+# VARIABLES
+x = 5
+name = "Hamza"
+is_Cool = True
+
+# PRINTING
+print("Hello")
+
+# ARRAYS -> LISTS
+arr = [1, 2, 3]
+
+
+# OBJECTS -> DICTIONARIES
+user = {
+    "name": "Adam",
+    "age": 18
+}
+
+print(user["name"])
+
+
+
+# IF STATEMENTS
+if x > 5:
+    print('big')
+else: 
+    print('small')
+
+
+# LOOPS
+
+for i in arr:
+    print(i)
+    
+# range loop, like classic js for loop
+for i in range(5): # 0 to 4
+    print(i)
+  
+    
+# FUNCTION
+def add(a, b):
+    return a + b
+
+
+# NONE vs NULL/UNDEFINED
+None
+
+# TRUTHY AND FALSY
+if not arr:
+    print("empty")
