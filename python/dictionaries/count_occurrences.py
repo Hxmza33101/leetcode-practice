d87d53ef46ee63d4bd6f8f474556d5bef2f9c8fe
Hxmza33101 <hkmughal2007@gmail.com>
2026-04-03 20:50:29 +1100

    Added something different to the count_occurrences problem in the python folder

--- python/dictionaries/count_occurrences.py
+++ python/dictionaries/count_occurrences.py
@@ -12,5 +12,16 @@ def count_occurrences(arr):
     print(count)
     return count    
 
+# Shorter way to do it using the built in .get() method in Python
+def count_occurrences1(arr):
+    count = {}
+    
+    for i in arr:
+        count[i] = count.get(i, 0) + 1
+    
+    print(count)
+    return count
+
+
 count_occurrences(['a','b','a'])  # → {'a': 2, 'b': 1}
-count_occurrences(['a','a','b','c','a','121'])  # → {'a': 2, 'b': 1}
+count_occurrences1(['a','a','b','c','a','121'])  # → {'a': 2, 'b': 1}
