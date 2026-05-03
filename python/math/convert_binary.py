81f63902cb41c546484cf2bec39872309ee4684b
Hxmza33101 <hkmughal2007@gmail.com>
2026-05-03 21:23:58 +1000

    Started code for convert_binary.py file

---
+++ python/math/convert_binary.py
@@ -0,0 +1,30 @@
+def convert():
+    print("What do you want to convert from?")
+    before = input("d(ecimal), b(inary) or h(exidecimal): ")
+    
+    print("\n What do you want to convert to?")
+    after = input("d(ecimal), b(inary) or h(exidecimal): ")
+    
+    number = input("\n Number to convert: ")
+    
+    
+    if before == after:
+        print(number)
+        return number
+    
+    return 0
+
+
+def bin_to_dec():
+
+def bin_to_hex():
+
+def dec_to_bin():
+    
+def dec_to_hex():
+
+def hex_to_dec():
+    
+def hex_to_bin():
+
+convert()
